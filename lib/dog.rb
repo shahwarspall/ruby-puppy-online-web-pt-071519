764d0b534d92997018103a859bba963326afa168
Shahwar Spall <shevy.spall@gmail.com>
2019-08-08 02:04:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,28 @@ class Dog
   @@all = []
   attr_accessor :name 
   
-  def initialize (name)
+  def initialize(name)
     @name = name
-    @@all << self 
+
+    save
   end
   
   def self.all 
     @@all 
   end 
   
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name 
+    end
+  end
+  
+end 
